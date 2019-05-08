243ffdcca92c15cf4517ea25c5c3054c18bde5ce
Tim Carew <mail@timcarew.com>
2019-05-08 10:05:12 -0230

    removed console logs

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ let head = document.getElementById("randColor");
 
 let randColor = () => {
   let randIndex = (Math.random() * 4);
-  console.log(randIndex);
   let colors = [
     "#FF2889",
     "#FF904D",
     "#825AFA",
     "#5271FF"
   ];
-  console.log(Math.floor(randIndex));
   return colors[Math.floor(randIndex)];
   
 }
