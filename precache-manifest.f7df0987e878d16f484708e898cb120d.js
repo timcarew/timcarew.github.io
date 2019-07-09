67a8f44bc957daf7236a68a06bc5112b7ce6e28c
Tim Carew <mail@timcarew.com>
2019-07-09 10:30:12 -0230

    unnuke

---
+++ precache-manifest.f7df0987e878d16f484708e898cb120d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48591e26fec1e2013a07050949bbd7e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "908a22e9fc80f0d9d079",
+    "url": "/static/css/main.f674f295.chunk.css"
+  },
+  {
+    "revision": "3804f646656499b62a66",
+    "url": "/static/js/2.dbeed2b9.chunk.js"
+  },
+  {
+    "revision": "908a22e9fc80f0d9d079",
+    "url": "/static/js/main.61e764a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "41b3e3a8a2fabd652951beacff1fa1ed",
+    "url": "/static/media/resume.41b3e3a8.pdf"
+  }
+]);
---
+++ src/components/SocialDrawer/SocialDrawer.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import resume from "../../resume.pdf";
+import "./SocialDrawer.css";
+
+class SocialDrawer extends Component {
+  render() {
+    let browser = "chrome";
+    return (
+      <div className="SocialDrawer">
+        <a
+          onMouseEnter={() =>
+            this.props.typeMessage('mail -s "Nice Site!" mail@timcarew.com')
+          }
+          href="mailto:mail@timcarew.com"
+          className="SocialDrawer-link"
+        >
+          Email
+        </a>
+        <a
+          onMouseEnter={() => this.props.typeMessage("open ./resume.pdf")}
+          href={resume}
+          className="SocialDrawer-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
+        </a>
+        <a
+          onMouseEnter={() =>
+            this.props.typeMessage(`${browser} https://timcarew.github.io`)
+          }
+          href="https://github.com/timcarew"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="SocialDrawer-link"
+        >
+          GitHub
+        </a>
+        <h4 href="" className="SocialDrawer-link SocialDrawer-link-static">
+          @
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default SocialDrawer;
